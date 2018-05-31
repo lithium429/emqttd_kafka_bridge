65dbe0a82a9988ccc346683633e09567d8e8606c
lithium429 <lithium429@163.com>
2018-05-31 17:54:51 +0800

    Update emqttd_kafka_bridge.erl

--- src/emqttd_kafka_bridge.erl
+++ src/emqttd_kafka_bridge.erl
@@ -90,7 +90,6 @@ on_message_publish(Message, _Env) ->
     io:format("publish ~s~n", [emqttd_message:format(Message)]),   
 
     From = Message#mqtt_message.from,
-    Sender =  Message#mqtt_message.sender,
     Topic = Message#mqtt_message.topic,
     Payload = Message#mqtt_message.payload, 
     QoS = Message#mqtt_message.qos,
